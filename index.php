e83e6ab139165ad0682a2c7931d9fcf319259ee1
RobertJGabriel <robert_gabriel@outlook.com>
2015-03-23 00:27:00 +0000

    First Commit :D

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+	
+	include('image_scraper.php');
+	$website_to_download_from = 'http://imgur.com/r/wallpapers';
+	$downloadImage = new image_Scraper($website_to_download_from);
+
+
+	//Please visit projectbird for more :)
+?>
